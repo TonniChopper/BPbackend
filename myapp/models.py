67d68f9cdb54967eea6fcf79b670dd28c41047d8
Andrii <programer1313@gmail.com>
2025-02-02 10:29:58 +0100

    Autenfication

--- myapp/models.py
+++ myapp/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings  # Используем ссылку на кастомную модель User
 import os
 
 class Graph(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Add this line
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
     pressure = models.FloatField()
     temperature = models.FloatField()
-    image = models.ImageField(upload_to='simulations/')
+    image = models.ImageField(upload_to='simulations/', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     simulation_id = models.IntegerField()
 
@@ -14,6 +14,8 @@ class Graph(models.Model):
         return f"Graph for pressure: {self.pressure}, temperature: {self.temperature}"
 
     def delete(self, *args, **kwargs):
-        if os.path.isfile(self.image.path):
-            os.remove(self.image.path)
+        if self.image and self.image.name:
+            image_path = self.image.path
+            if os.path.isfile(image_path):
+                os.remove(image_path)
         super().delete(*args, **kwargs)
